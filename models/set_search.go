f91b526ebdee7ab34b7bc334a3aa2d792ef3e56d
전강현 <aniren@debian.moe>
2023-05-28 04:11:56 +0900

    Update set_search.go

--- models/set_search.go
+++ models/set_search.go
@@ -136,7 +136,10 @@ func SearchSets(db, searchDB *sql.DB, opts SearchOptions) ([]Set, error) {
 
 		setIDsQuery += " ORDER BY MATCH(artist, title, creator, source, tags) AGAINST('" + mysqlStringReplacer.Replace(opts.Query) + "') DESC, id DESC " + limit
 
-		//fmt.Println("setIDsQuery = ", setIDsQuery)
+		fmt.Print("\n")
+		fmt.Println("setIDsQuery = ", setIDsQuery)
+		fmt.Print("\n")
+
 		limit = ""
 
 		// fetch rows
