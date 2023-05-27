a701211303f87971ad9525669e89626347926b87
전강현 <aniren@debian.moe>
2023-05-28 00:32:35 +0900

    Update cheesegull.bat

--- cheesegull.bat
+++ cheesegull.bat
@@ -3,3 +3,4 @@ B:
 cd B:\redstar\cheesegull
 cheesegull.exe -k 4713134cf26236be8cdad80768b50168feadf56f -u "Remember Debian" -p skchqhdpdy0113 -m osu:821059401241@(127.0.0.1:3306)/cheesegull --search-dsn osu:821059401241@(127.0.0.1:3306)/cheesegull -a 0.0.0.0:62011 --secret-ci ILoveDebian
 pause
+a
