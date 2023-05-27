5c804a96646fde2fe9e68ad64669f28d830c70ba
전강현 <aniren@debian.moe>
2023-05-27 23:30:14 +0900

    Initial commit

---
+++ cheesegull.bat
@@ -0,0 +1,5 @@
+@echo off
+B:
+cd B:\cheesegull
+cheesegull.exe -k 4713134cf26236be8cdad80768b50168feadf56f -u remember_debian -p skchqhdpdy0113 -m root@/cheesegull --search-dsn root@/cheesegull -a 0.0.0.0:62011 --secret-ci ILoveDebian
+pause
